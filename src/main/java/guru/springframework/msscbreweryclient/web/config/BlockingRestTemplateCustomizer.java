d93d9a95d38d0d98b05c9fd6209b6cb91b2890f4
tomvde <D-8110tr>
2020-01-06 18:48:10 +0100

    externalize properties into properties file

--- src/main/java/guru/springframework/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
@@ -5,6 +5,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -13,6 +14,22 @@ import org.springframework.web.client.RestTemplate;
 
 @Component
 public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
+
+    private final Integer maxTotalConnections;
+    private final Integer defaultMaxTotalConnections;
+    private final Integer connectionRequestTimeout;
+    private final Integer socketTimeout;
+
+    public BlockingRestTemplateCustomizer(@Value("${sfg.maxtotalconnections}") Integer maxTotalConnections,
+                                          @Value("${sfg.defaultmaxtotalconnections}") Integer defaultMaxTotalConnections,
+                                          @Value("${sfg.connectionrequesttimeout}") Integer connectionRequestTimeout,
+                                          @Value("${sfg.sockettimeout}") Integer socketTimeout) {
+        this.maxTotalConnections = maxTotalConnections;
+        this.defaultMaxTotalConnections = defaultMaxTotalConnections;
+        this.connectionRequestTimeout = connectionRequestTimeout;
+        this.socketTimeout = socketTimeout;
+    }
+
     @Override
     public void customize(RestTemplate restTemplate) {
         restTemplate.setRequestFactory(this.clientHttpRequestFactory());
@@ -20,13 +37,13 @@ public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
 
     private ClientHttpRequestFactory clientHttpRequestFactory() {
         PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
-        connectionManager.setMaxTotal(100); //maximum 100 connections
-        connectionManager.setDefaultMaxPerRoute(20); //maximum 20 connections per route
+        connectionManager.setMaxTotal(maxTotalConnections); //maximum 100 connections
+        connectionManager.setDefaultMaxPerRoute(defaultMaxTotalConnections); //maximum 20 connections per route
 
         RequestConfig requestConfig = RequestConfig
                 .custom()
-                .setConnectionRequestTimeout(3000)
-                .setSocketTimeout(3000)
+                .setConnectionRequestTimeout(connectionRequestTimeout)
+                .setSocketTimeout(socketTimeout)
                 .build();
 
         CloseableHttpClient httpClient = HttpClients
